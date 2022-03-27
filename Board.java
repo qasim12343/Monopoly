bb76b9928c1a3613d3adea8b1ed8dda2a2052b06
Qasim <qasem.yousifi20@gmail.com>
2022-03-27 16:51:40 +0430

    reformed

--- Board.java
+++ Board.java
@@ -40,9 +40,9 @@ class test {
         }
         b1 = new Board(size);
 
-        if (input.next().equals("start_game")){
+        if (input.next().equals("start_game")) {
             int countRound = 1;
-            System.out.println("round "+countRound);
+            System.out.println("round " + countRound);
 
             int turn = 0;
             do {
@@ -55,77 +55,133 @@ class test {
                 }
                 b1.player[turn].numberOfCell += dice;
 
-                if (b1.player[turn].numberOfCell > 24){
+                if (b1.player[turn].numberOfCell > 24) {
                     countRound++;
                     b1.player[turn].numberOfCell = 1;
                 }
 
-                switch (b1.player[turn].numberOfCell){
+                switch (b1.player[turn].numberOfCell) {
                     case 1:
                         System.out.println("Enter your command: 1-build, 2-buy, 3-sell, 4-fly\n" +
                                 "5-free, 6-invest, 7-index, 8-property, 9-time, 10-rank");
-                        switch (input.nextInt()){
-                            case 1: break;
-                            case 2: break;
-                            case 3: break;
-                            case 4: break;
-                            case 5: break;
-                            case 6: break;
-                            case 7: break;
-                            case 8: break;
-                            case 9: break;
-                            case 10: break;
+                        switch (input.nextInt()) {
+                            case 1:
+                                break;
+                            case 2:
+                                break;
+                            case 3:
+                                break;
+                            case 4:
+                                break;
+                            case 5:
+                                break;
+                            case 6:
+                                break;
+                            case 7:
+                                break;
+                            case 8:
+                                break;
+                            case 9:
+                                break;
+                            case 10:
+                                break;
 
                             default:
                                 System.out.println("wrong command");
                                 break;
                         }
                         break;
-                    case 2: b1.player[turn].balance -= 100; break;
-                    case 3: b1.player[turn].balance -= 300; break;
-                    case 4: b1.player[turn].balance -= 400; break;
-                    case 5: b1.player[turn].balance -= 500; break;
-                    case 6: b1.player[turn].balance -= 110; break;
-                    case 7: b1.player[turn].balance -= 120; break;
-                    case 8: b1.player[turn].balance -= 140; break;
-                    case 9: b1.player[turn].balance -= 10; break;
-                    case 10: b1.player[turn].balance -= 1; break;
-                    case 11: b1.player[turn].balance -= 103; break;
-                    case 12: b1.player[turn].balance -= 105; break;
-                    case 13: b1.player[turn].balance -= 101; break;
-                    case 14: b1.player[turn].balance -= 301; break;
-                    case 15: b1.player[turn].balance -= 40; break;
-                    case 16: b1.player[turn].balance -= 50; break;
-                    case 17: b1.player[turn].balance -= 60; break;
-                    case 18: b1.player[turn].balance -= 70; break;
-                    case 19: b1.player[turn].balance -= 80; break;
-                    case 20: b1.player[turn].balance -= 90; break;
-                    case 21: break;
-                    case 22: b1.player[turn].balance -= 100; break;
-                    case 23: b1.player[turn].balance -= 100; break;
-                    case 24: b1.player[turn].balance -= 100; break;
+                    case 2:
+                        b1.player[turn].balance -= 100;
+                        break;
+                    case 3:
+                        b1.player[turn].balance -= 300;
+                        break;
+                    case 4:
+                        b1.player[turn].balance -= 400;
+                        break;
+                    case 5:
+                        b1.player[turn].balance -= 500;
+                        break;
+                    case 6:
+                        b1.player[turn].balance -= 110;
+                        break;
+                    case 7:
+                        b1.player[turn].balance -= 120;
+                        break;
+                    case 8:
+                        b1.player[turn].balance -= 140;
+                        break;
+                    case 9:
+                        b1.player[turn].balance -= 10;
+                        break;
+                    case 10:
+                        b1.player[turn].balance -= 1;
+                        break;
+                    case 11:
+                        b1.player[turn].balance -= 103;
+                        break;
+                    case 12:
+                        b1.player[turn].balance -= 105;
+                        break;
+                    case 13:
+                        b1.player[turn].balance -= 101;
+                        break;
+                    case 14:
+                        b1.player[turn].balance -= 301;
+                        break;
+                    case 15:
+                        b1.player[turn].balance -= 40;
+                        break;
+                    case 16:
+                        b1.player[turn].balance -= 50;
+                        break;
+                    case 17:
+                        b1.player[turn].balance -= 60;
+                        break;
+                    case 18:
+                        b1.player[turn].balance -= 70;
+                        break;
+                    case 19:
+                        b1.player[turn].balance -= 80;
+                        break;
+                    case 20:
+                        b1.player[turn].balance -= 90;
+                        break;
+                    case 21:
+                        break;
+                    case 22:
+                        b1.player[turn].balance -= 100;
+                        break;
+                    case 23:
+                        b1.player[turn].balance -= 100;
+                        break;
+                    case 24:
+                        b1.player[turn].balance -= 100;
+                        break;
                 }
-                System.out.println(b1.player[turn].name+"'s balance: "+b1.player[turn].balance);
-                System.out.println(b1.player[turn].name+"'s cell number: "+b1.player[turn].numberOfCell+"\n");
+                System.out.println(b1.player[turn].name + "'s balance: " + b1.player[turn].balance);
+                System.out.println(b1.player[turn].name + "'s cell number: " + b1.player[turn].numberOfCell + "\n");
                 turn++;
                 if (turn >= b1.sizeOfPlayer)
                     turn = 0;
-            }while (winner());
+            } while (winner());
         }
     }
-    public static boolean winner(){
+
+    public static boolean winner() {
 
         for (int i = 0; i < b1.sizeOfPlayer; i++) {
-            if(b1.player[i].balance <= 10){
+            if (b1.player[i].balance <= 10) {
                 b1.sizeOfPlayer--;
-                System.out.println("Player "+(i+1)+" lost\n");
+                System.out.println("Player " + (i + 1) + " lost\n");
                 for (int j = i; j < b1.sizeOfPlayer; j++) {
-                    b1.player[j] = b1.player[j+1];
+                    b1.player[j] = b1.player[j + 1];
                 }
             }
         }
-        if(b1.sizeOfPlayer == 1){
-            System.out.println(b1.player[0].name+" is winner");
+        if (b1.sizeOfPlayer == 1) {
+            System.out.println(b1.player[0].name + " is winner");
             return false;
         }
         return true;
